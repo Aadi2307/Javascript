265d918259b7b274b9a6db2ee1ae515679a24486
Aadi2307 <95700745+Aadi2307@users.noreply.github.com>
2022-02-15 12:30:23 +0530

    Update document.js
    Added by Yash

--- js/document.js
+++ js/document.js
@@ -1,4 +1,5 @@
 document.writeln("JavaScript is a simple language for javatpoint learners ");
 document.writeln("document is class")
 document.write("and write is method")
+document.write("and write is method 2")
 alert("hello everyone");
